29f8db89ae636b3cfe4403e673f8b243d086b511
Chanel Morgan <chanelmorgan05@icloud.com>
2024-01-25 21:42:05 +0000

    Added the score function
    TO-DO: Collision detection
    TO-DO: Game over screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+import random
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -26,6 +27,8 @@ start_image = pygame.image.load("assets/start.png")
 # higher the number the faster the ground will be moving
 scroll_speed = 1
 bird_start_position = (100, 250)
+score = 0
+font = pygame.font.SysFont('Segoe', 26)
 
 
 class Bird(pygame.sprite.Sprite):
@@ -40,10 +43,12 @@ class Bird(pygame.sprite.Sprite):
         self.vel = 0
         # Prevents the player from spamming the space bar
         self.flap = False
+        self.alive = True
 
     def update(self, user_input):
         # Animate Bird
-        self.image_index += 1
+        if self.alive:
+            self.image_index += 1
         # When the image index gets to 30 reset
         if self.image_index >= 30:
             self.image_index = 0
@@ -57,10 +62,42 @@ class Bird(pygame.sprite.Sprite):
             self.rect.y += int(self.vel)
         if self.vel == 0:
             self.flap = False
+
+        # Rotate Bird
+        self.image = pygame.transform.rotate(self.image, self.vel * -7)
         # User input
-        if user_input[pygame.K_SPACE] and not self.flap and self.rect.y > 0:
+        if user_input[pygame.K_SPACE] and not self.flap and self.rect.y > 0 and self.alive:
             self.flap = True
             self.vel = -7
+
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, image, pipe_type):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+        self.enter, self.exit, self.passed = False, False, False
+        self.pipe_type = pipe_type
+
+    def update(self):
+        # Responsible for moving the pipes from the RHS to the LHS
+        self.rect.x -= scroll_speed
+
+        # Delete the pipe as soon as it moves of the screen
+        if self.rect.x <= -win_width:
+            self.kill()
+
+        # Score
+        global score
+        if self.pipe_type == 'bottom':
+            if bird_start_position[0] > self.rect.topleft[0] and not self.passed:
+                self.enter = True
+            if bird_start_position[0] > self.rect.topright[0] and not self.passed:
+                self.exit = True
+            if self.enter and self.exit and not self.passed:
+                self.passed = True
+                score += 1
 
 
 class Ground(pygame.sprite.Sprite):
@@ -87,9 +124,16 @@ def quit_game():
 
 # Game Main Method
 def main():
+
+    global score
+
     # Instantiate Bird
     bird = pygame.sprite.GroupSingle()
     bird.add(Bird())
+
+    # Setup Pipes
+    pipe_timer = 0
+    pipes = pygame.sprite.Group()
 
     # Instantiate Initial Ground
     x_pos_ground, y_pos_ground = 0, 520
@@ -116,13 +160,30 @@ def main():
             ground.add(Ground(win_width, y_pos_ground))
 
         # Draw - Pipes, Ground and Bird to start game
+        pipes.draw(window)
         ground.draw(window)
         bird.draw(window)
 
+        # Show score
+        score_text = font.render('Score: ' + str(score), True, pygame.Color(255, 255, 255))
+        window.blit(score_text, (28, 28))
+
         # Update - Pipes, Ground and Bird
         # Need to update the ground so its moves
-        ground.update()
+        if bird.sprite.alive:
+            pipes.update()
+            ground.update()
         bird.update(user_input)
+
+        # Spawn Pipes
+        if pipe_timer <= 0 and bird.sprite.alive:
+            x_top, x_bottom = 550, 550
+            y_top = random.randint(-600, -488)
+            y_bottom = y_top + random.randint(90, 130) + bottom_pipe_image.get_height()
+            pipes.add(Pipe(x_top, y_top, top_pipe_image, 'top'))
+            pipes.add(Pipe(x_bottom, y_bottom, bottom_pipe_image, 'bottom'))
+            pipe_timer = random.randint(180, 250)
+        pipe_timer -= 1
 
         # frames per second limited to 60
         clock.tick(60)
